dfa614401021a97aa2704e41777ef0970dd7c315
Laboratorio <laboratorio@pop-os.localdomain>
2019-11-11 09:30:56 -0300

    Recebendo e validando dados do formulário

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,14 @@
 module.exports.iniciaChat = function (application, req, res) {
+    //const dadosForm = req.body;
+
+    req.assert('apelido', 'Nome ou apelido é obriagatório').notEmpty();
+    req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+
+    const errors = req.validationErrors();
+
+    if (errors) {
+        res.render('index', {validacao: errors});
+        return;
+    }
     res.render('chat');
 };
